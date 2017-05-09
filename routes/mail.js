2b6792f7ac3502108d149731a783cba1ea02fe9d
--global <--global>
2017-05-09 16:02:55 -0500

    step3

--- routes/mail.js
+++ routes/mail.js
@@ -3,23 +3,21 @@ const nodemailer= require('nodemailer')
 
 exports.sendMail= function(req,res) {
 	let transporter= nodemailer.createTransport({
-		service: 'gmail'
+		service: 'Gmail',
 		auth: {
 			user: 'sissyyayle0517@gmail.com',
 			pass:'chisqueado0517'
 		}
 	})
-}
-
 let mailOptions= {
-	from: name 
-	to: 'fgme.facturacion@b-and-bconsultores.com.mx'
-	subject: phone
+	from: name, 
+	to: 'fgme.facturacion@b-and-bconsultores.com.mx',
+	subject: phone,
 	text: message
 }
 // Envío de email
 
-transport.sendMail(mailOptions, function(error, info) {
+transporter.sendMail(mailOptions, function(error, info) {
 	if(error){
 		console.log(error)
 		res.send(500, err.message)
@@ -29,4 +27,6 @@ transport.sendMail(mailOptions, function(error, info) {
 		res.status(200).jsonp(req.body)
 	}
 })
+}
+
 
